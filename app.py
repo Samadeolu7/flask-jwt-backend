21bfa042ce55289e796b62dd1894fd7498b218c7
Samuel <samadeolu7@gmail.com>
2023-12-18 13:37:48 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+import jwt
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+CORS(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    user = User(username=data['username'], password=hashed_password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully'})
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+
+    if user and bcrypt.check_password_hash(user.password, data['password']):
+        token = jwt.encode({'user_id': user.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+                          app.config['SECRET_KEY'], algorithm='HS256')
+        return jsonify({'token': token.decode('utf-8')})
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
